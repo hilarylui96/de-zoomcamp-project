fa2b41ebe66b827f2ace6674e0c3b11be4fc87d7
Hilary Lui <hilarylui96@gmail.com>
2025-03-31 13:27:31 -0700

    fixed terraform error

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,8 +39,9 @@ resource "google_project_iam_member" "terraform_sa_roles" {
   "roles/storage.admin",
   "roles/bigquery.admin",
   "roles/editor",
-  "roles/iam.serviceAccountUser",
   "roles/resourcemanager.projectIamAdmin",
+  "roles/iam.serviceAccountAdmin",
+  "roles/iam.securityAdmin"
   ])
   project = var.project
   role    = each.value
