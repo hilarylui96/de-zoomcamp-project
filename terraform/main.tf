105564a055521ded76343bb775c8c8787d363568
Hilary Lui <hilarylui96@gmail.com>
2025-03-31 11:39:16 -0700

    updated bucket name

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "demo-bucket" {
-  name          = var.gcs_bucket_name
+  name          = var.gcs_test_bucket_name
   location      = var.location
   force_destroy = true
 
