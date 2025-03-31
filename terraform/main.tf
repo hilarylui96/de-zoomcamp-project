e41d995783e07252a5e3782936cfab8ce19297d1
Hilary Lui <hilarylui96@gmail.com>
2025-03-31 11:43:43 -0700

    updated terraform settings

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,5 @@
 resource "google_storage_bucket" "demo-bucket" {
   name          = var.gcs_test_bucket_name
   location      = var.location
-  force_destroy = true
-
-
-  lifecycle_rule {
-    condition {
-      age = 1
-    }
-    action {
-      type = "AbortIncompleteMultipartUpload"
-    }
-  }
+  force_destroy = false
 }
