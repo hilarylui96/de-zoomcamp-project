083b0ac6ad9d1a9bba7068203909d5818da7e8f4
Hilary Lui <hilarylui96@gmail.com>
2025-03-31 11:50:24 -0700

    added terraform providers

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.6.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials)
+  project     = var.project
+  region      = var.region
+}
+
 resource "google_storage_bucket" "demo-bucket" {
   name          = var.gcs_test_bucket_name
   location      = var.location
